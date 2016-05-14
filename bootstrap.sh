10589f64041fbf82b9ab58600eafb9a08ecc0137
yendry <jendry0017@gmail.com>
2016-05-14 10:31:49 -0430

    actualizacion
    commint 1

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,11 +3,10 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install nodejs npm -y 
-cd /vagrant
 sudo npm install -g gulp
 sudo npm cache clean -f
 sudo npm install -g n
 sudo n 4.4.0
 sudo update-alternatives --install /usr/bin/node nodejs /usr/local/n/versions/node/4.4.0/bin/node 100
 npm install
-npm install -g eslint
+sudo npm install -g eslint
